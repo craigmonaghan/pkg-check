c8d5dac71b8fc7815940574e3486d39da0874f4d
craigmonaghan <craigmonaghan.op@gmail.com>
2025-11-30 22:13:18 +0000

    implemented typer for the cli

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import typer
 from rich.console import Console
 from rich.panel import Panel
-from core.checkers.npm_checker import NPMChecker
+from core.npm_checker import NPMChecker
 
 
 app = typer.Typer()
@@ -10,4 +10,30 @@ console = Console()
 
 @app.command()
 def npm(package_name: str):
-    pass
+    console.print(f"🔍 Checking [cyan]{package_name}[/cyan] on NPM..")
+    try:
+        checker = NPMChecker()
+        pkg =checker.check_package(package_name)
+        
+        output = f"""[bold cyan]NPM Package Info[/bold cyan]
+        
+        🔍 Name: {pkg.name}
+        📦 Version: {pkg.version}
+        👤 Maintainer(s): {pkg.maintainer}
+        📅 Updated: {pkg.last_updated}
+        """
+        
+        console.print(Panel(output, border_style="green"))
+    except Exception as e:
+        console.print(f"[red]❌ Error: {e}[/red]")
+
+@app.command()
+def github(package_name: str):
+    console.print(f"[yellow]Not implemented[/yellow]") 
+        
+if __name__ == "__main__":
+    app()
+        
+    
+    
+        
