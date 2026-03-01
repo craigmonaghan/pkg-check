9d081fa251de31d0a9c254366aab7cb49298fb5d
craigmonaghan <craigmonaghan.op@gmail.com>
2026-03-01 20:47:33 +0000

    finished AUR checker

---
+++ core/aur_checker.py
@@ -0,0 +1,64 @@
+from typing import List
+import requests
+from datetime import datetime, timezone
+from core.checker_base import CheckerBase
+from core.package import Package
+from parsers import exporter
+
+
+class AURChecker(CheckerBase):
+    def check_package(self, package_name: str) -> Package:
+        self.package_name = package_name
+        try:
+            url = f"https://aur.archlinux.org/rpc/v5/info?arg[]={package_name}"
+            response = requests.get(url, timeout=10)
+            if response.status_code == 404:
+                raise Exception(f"Package '{package_name}' not found on the AUR")
+            
+            response.raise_for_status()
+            self.data = response.json()
+            
+            result = self.data['results'][0]
+            version = result.get('Version', {})
+            
+
+            last_modified = result.get('LastModified')
+            last_updated = datetime.fromtimestamp(last_modified, tz=timezone.utc) if last_modified else None
+            
+            
+            package_url = f"https://aur.archlinux.org/packages/{package_name}"
+            maintainer = result.get('Maintainer')
+
+            return Package(
+                name=package_name,
+                source="AUR",
+                version=version,
+                last_updated=last_updated,
+                maintainer=maintainer,
+                url=package_url
+            ) 
+            
+            
+        except requests.exceptions.HTTPError as e:
+            raise Exception(f"Package '{package_name}' not found on AUR") from e
+        
+        except requests.exceptions.Timeout:
+            raise Exception(f"Request timed out while checking '{package_name}'")
+        
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Network error while checking '{package_name}': {e}")
+
+        except KeyError as e:
+            raise Exception(f"Unexpected response format from AUR for '{package_name}'")
+
+    def get_data(self, request):
+        request = self.data
+        if request is not None:
+            return request
+    
+    def get_parserered(self, package_name):
+        package_name = package_name
+        return exporter.export(package_name, AURChecker(), None)
+
+    def list_installed(self) -> List[str]:
+        return []
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from rich.console import Console
 from rich.panel import Panel
 from core.npm_checker import NPMChecker
 from core.pypi_checker import PyPiChecker
+from core.aur_checker import AURChecker
 from parsers.exporter import export
 
 
@@ -51,13 +52,35 @@ def pypi(package_name: str):
         
 @app.command()
 def aur(package_name: str):
-    pass
+    console.print(f"Checking [cyan]{package_name}[/cyan] on the AUR..")
+    try:
+        checker = AURChecker()
+        pkg =checker.check_package(package_name)        
+        output = f"""[bold cyan]AUR Package Info[/bold cyan]
+        
+        Name: {pkg.name}
+        Version: {pkg.version}
+        Maintainer(s): {pkg.maintainer}
+        Updated: {pkg.updated_display()}
+        URL: {pkg.url}
+        """
+        
+        console.print(Panel(output, border_style="green"))
+    except Exception as e:
+        console.print(f"[red]Error: {e}[/red]")
+
 
 @app.command()
 def fetch(checker, package_name, output_file: Optional[str] = None):
     console.print(f"writing response for [cyan]{package_name}[/cyan]")
     try:
-        checker = PyPiChecker if checker == "pypi" else NPMChecker
+        if checker == "pypi": 
+            checker = PyPiChecker 
+        elif checker == "npm":
+            checker = NPMChecker
+        else:
+            checker = AURChecker
+            
         if output_file is None:
             output_file = f"response_{package_name}.json"
         output = export(checker, package_name, output_file)
